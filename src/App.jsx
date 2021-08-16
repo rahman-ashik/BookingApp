8e2731795df795ec5ddca87d76c388f0ecc75e37
Mushfiqur Rahman <rahman.mushfiq2000@gmail.com>
2021-08-17 00:15:27 +0600

    added Bootstarp & Tailwind, replaced react-scripts with craco. eject script still uses react-scripts. Created a global top Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+    </div>
+  );
+};
+
+export default App;
